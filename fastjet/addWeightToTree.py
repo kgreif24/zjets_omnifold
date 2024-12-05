44f8d77cb567b97282d347e4e943c4ebf1eae985
kgreif <kevingreif24@gmail.com>
2024-12-05 07:00:05 -0800

    Lint add weight utility

--- fastjet/addWeightToTree.py
+++ fastjet/addWeightToTree.py
@@ -4,28 +4,33 @@ is that we need to duplicate a huge amount of data, which is not ideal. Should p
 the optimal way to do this.
 """
 
-
 import argparse
 import uproot
-import numpy as np 
+import numpy as np
 
 
 def main():
 
-    parser = argparse.ArgumentParser(description='Add weights to MC tree')
-    parser.add_argument('--input', type=str, default='/global/cfs/cdirs/m3246/ZjetOmnifold/data/slimmed_files/WithTracks_ZjetOmnifold_May19_MGPy8FxFxRew_syst_test_Mar0723.root', help='Input file')
-    parser.add_argument('--output', type=str, help='Output file')
-    parser.add_argument('--weights', type=str, help='Weights file')
-    parser.add_argument('--get_train', action='store_true', help='Append train weights instead of test')
+    parser = argparse.ArgumentParser(description="Add weights to MC tree")
+    parser.add_argument(
+        "--input",
+        type=str,
+        default="/global/cfs/cdirs/m3246/ZjetOmnifold/data/slimmed_files/WithTracks_ZjetOmnifold_May19_MGPy8FxFxRew_syst_test_Mar0723.root",
+        help="Input file",
+    )
+    parser.add_argument("--output", type=str, help="Output file")
+    parser.add_argument("--weights", type=str, help="Weights file")
+    parser.add_argument(
+        "--get_train", action="store_true", help="Append train weights instead of test"
+    )
     args = parser.parse_args()
 
     fileMC = uproot.open(args.input)
-    treeMC = fileMC['OmniTree']
-        
-    branches_mc = {name: treeMC[name].array() for name in treeMC.keys()}
-    weights = np.load(args.weights)['train' if args.get_train else 'test']
-    branches_mc["omni_weight"] = weights
+    treeMC = fileMC["OmniTree"]
 
+    branches_mc = {name: treeMC[name].array() for name in treeMC.keys()}
+    weights = np.load(args.weights)["train" if args.get_train else "test"]
+    branches_mc["omni_weight"] = weights
 
     with uproot.recreate(args.output) as new_file:
         new_file["OmniTree"] = branches_mc
@@ -35,5 +40,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    pass 
-
+    pass
