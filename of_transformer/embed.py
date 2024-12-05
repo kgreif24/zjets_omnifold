5df7233bd9b5c501e35cb3e1ddc747acc88ff79f
Kevin Greif <kevingreif24@gmail.com>
2024-12-05 10:32:22 +0100

    Lint of transformer code

--- of_transformer/embed.py
+++ of_transformer/embed.py
@@ -1,22 +1,26 @@
-import torch
 import torch.nn as nn
 
 from . import input_distributed
 
 # Should "input_dim" here be the number of particle features or the maximum
-# number of particles? I think the latter, and then we mask after the embedding layer.
+# number of particles? I think the latter, and then we mask after the
+# embedding layer.
+
+
 class Embed(nn.Module):
-    def __init__(self, input_dim, dims, normalize_input=True, activation='gelu'):
+    def __init__(self, input_dim, dims, normalize_input=True, activation="gelu"):
         super().__init__()
 
         self.input_bn = nn.BatchNorm1d(input_dim) if normalize_input else None
         module_list = []
         for dim in dims:
-            module_list.extend([
-                nn.Linear(input_dim, dim),
-                # nn.BatchNorm1d(dim),
-                nn.GELU() if activation == 'gelu' else nn.ReLU(),
-            ])
+            module_list.extend(
+                [
+                    nn.Linear(input_dim, dim),
+                    # nn.BatchNorm1d(dim),
+                    nn.GELU() if activation == "gelu" else nn.ReLU(),
+                ]
+            )
             input_dim = dim
         embed_one_element = nn.Sequential(*module_list)
         self.embed = input_distributed.InputDistributed(embed_one_element)
--- of_transformer/pair_embed.py
+++ of_transformer/pair_embed.py
@@ -4,12 +4,21 @@ from functools import partial
 
 from . import net_utils
 
+
 class PairEmbed(nn.Module):
     def __init__(
-            self, pairwise_lv_dim, pairwise_input_dim, dims,
-            remove_self_pair=False, use_pre_activation_pair=True, mode='sum',
-            normalize_input=True, activation='gelu', eps=1e-8,
-            for_onnx=False):
+        self,
+        pairwise_lv_dim,
+        pairwise_input_dim,
+        dims,
+        remove_self_pair=False,
+        use_pre_activation_pair=True,
+        mode="sum",
+        normalize_input=True,
+        activation="gelu",
+        eps=1e-8,
+        for_onnx=False,
+    ):
         super().__init__()
 
         self.pairwise_lv_dim = pairwise_lv_dim
@@ -18,36 +27,41 @@ class PairEmbed(nn.Module):
         self.remove_self_pair = remove_self_pair
         self.mode = mode
         self.for_onnx = for_onnx
-        self.pairwise_lv_fts = partial(net_utils.pairwise_lv_fts, num_outputs=pairwise_lv_dim, eps=eps, for_onnx=for_onnx)
+        self.pairwise_lv_fts = partial(
+            net_utils.pairwise_lv_fts,
+            num_outputs=pairwise_lv_dim,
+            eps=eps,
+            for_onnx=for_onnx,
+        )
         self.out_dim = dims[-1]
 
-        if self.mode == 'concat':
+        if self.mode == "concat":
             input_dim = pairwise_lv_dim + pairwise_input_dim
             module_list = [nn.BatchNorm1d(input_dim)] if normalize_input else []
             for dim in dims:
-                module_list.extend([
-                    # Why use a convolution here? Shouldn't you use a linear layer?
-                    # No, we want to share weights for each element in the sequence, but not for each pairwise dim
-                    # Conv1d acts as (N, C_in, L) -> (N, C_out, L) where L is the length of the sequence
-                    # The pairwise dims are in the 1st dimension, so this is exactly what we want.
-                    nn.Conv1d(input_dim, dim, 1),
-                    nn.BatchNorm1d(dim),
-                    nn.GELU() if activation == 'gelu' else nn.ReLU(),
-                ])
+                module_list.extend(
+                    [
+                        nn.Conv1d(input_dim, dim, 1),
+                        nn.BatchNorm1d(dim),
+                        nn.GELU() if activation == "gelu" else nn.ReLU(),
+                    ]
+                )
                 input_dim = dim
             if use_pre_activation_pair:
                 module_list = module_list[:-1]
             self.embed = nn.Sequential(*module_list)
-        elif self.mode == 'sum':
+        elif self.mode == "sum":
             if pairwise_lv_dim > 0:
                 input_dim = pairwise_lv_dim
                 module_list = [nn.BatchNorm1d(input_dim)] if normalize_input else []
                 for dim in dims:
-                    module_list.extend([
-                        nn.Conv1d(input_dim, dim, 1),
-                        nn.BatchNorm1d(dim),
-                        nn.GELU() if activation == 'gelu' else nn.ReLU(),
-                    ])
+                    module_list.extend(
+                        [
+                            nn.Conv1d(input_dim, dim, 1),
+                            nn.BatchNorm1d(dim),
+                            nn.GELU() if activation == "gelu" else nn.ReLU(),
+                        ]
+                    )
                     input_dim = dim
                 if use_pre_activation_pair:
                     module_list = module_list[:-1]
@@ -57,43 +71,40 @@ class PairEmbed(nn.Module):
                 input_dim = pairwise_input_dim
                 module_list = [nn.BatchNorm1d(input_dim)] if normalize_input else []
                 for dim in dims:
-                    module_list.extend([
-                        nn.Conv1d(input_dim, dim, 1),
-                        nn.BatchNorm1d(dim),
-                        nn.GELU() if activation == 'gelu' else nn.ReLU(),
-                    ])
+                    module_list.extend(
+                        [
+                            nn.Conv1d(input_dim, dim, 1),
+                            nn.BatchNorm1d(dim),
+                            nn.GELU() if activation == "gelu" else nn.ReLU(),
+                        ]
+                    )
                     input_dim = dim
                 if use_pre_activation_pair:
                     module_list = module_list[:-1]
                 self.fts_embed = nn.Sequential(*module_list)
         else:
-            raise RuntimeError('`mode` can only be `sum` or `concat`')
+            raise RuntimeError("`mode` can only be `sum` or `concat`")
 
     def forward(self, x, uu=None):
-        """ forward - Why does the forward pass for pair embedding take two inputs, x and uu?
-        Maybe I can just leave uu unused, since I see no reason to include it.
-
-        I think it's a way to input features into the network that are not calculated via the
-        "pairwise_lv_fts" function below. Indeed this is definitely correct.
-
-        For my use case I should be able to just use the x input and ignore the uu input.
-        """
-
 
         # x: (batch, v_dim, seq_len)
         # uu: (batch, v_dim, seq_len, seq_len)
-        assert (x is not None or uu is not None)
+        assert x is not None or uu is not None
 
-        # Do not evaluate gradients here as there are no trainable weights in the calculation of 
-        # pairwise features.
+        # Do not evaluate gradients here as there are no trainable weights
+        # in the calculation of pairwise features.
         with torch.no_grad():
             if x is not None:
                 batch_size, _, seq_len = x.size()
             else:
                 batch_size, _, seq_len, _ = uu.size()
             if self.is_symmetric and not self.for_onnx:
-                i, j = torch.tril_indices(seq_len, seq_len, offset=-1 if self.remove_self_pair else 0,
-                                          device=(x if x is not None else uu).device)
+                i, j = torch.tril_indices(
+                    seq_len,
+                    seq_len,
+                    offset=-1 if self.remove_self_pair else 0,
+                    device=(x if x is not None else uu).device,
+                )
                 if x is not None:
                     x = x.unsqueeze(-1).repeat(1, 1, 1, seq_len)
                     xi = x[:, :, i, j]  # (batch, dim, seq_len*(seq_len+1)/2)
@@ -111,17 +122,18 @@ class PairEmbed(nn.Module):
                     x = x.view(-1, self.pairwise_lv_dim, seq_len * seq_len)
                 if uu is not None:
                     uu = uu.view(-1, self.pairwise_input_dim, seq_len * seq_len)
-            if self.mode == 'concat':
+            if self.mode == "concat":
                 if x is None:
                     pair_fts = uu
                 elif uu is None:
-                    pair_fts = x  # (batch, pairwise_lv_dim, seq_len*(seq_len-1)/2) <- assuming we remove self pair
+                    # Shape assumes we remove self pair
+                    pair_fts = x  # (batch, pairwise_lv_dim, seq_len*(seq_len-1)/2)
                 else:
                     pair_fts = torch.cat((x, uu), dim=1)
 
-        if self.mode == 'concat':
+        if self.mode == "concat":
             elements = self.embed(pair_fts)  # (batch, embed_dim, num_elements)
-        elif self.mode == 'sum':
+        elif self.mode == "sum":
             if x is None:
                 elements = self.fts_embed(uu)
             elif uu is None:
@@ -129,11 +141,18 @@ class PairEmbed(nn.Module):
             else:
                 elements = self.embed(x) + self.fts_embed(uu)
 
-        # If the features are symmetric, we don't want to waste time calculating the same features
-        # twice, so we just calculate the lower triangular part of the matrix and then fill in the
-        # upper triangular part with the same values.
+        # If the features are symmetric, we don't want to waste time calculating
+        # the same features twice, so we just calculate the lower triangular part
+        # of the matrix and then fill in the upper triangular part with the same values.
         if self.is_symmetric and not self.for_onnx:
-            y = torch.zeros(batch_size, self.out_dim, seq_len, seq_len, dtype=elements.dtype, device=elements.device)
+            y = torch.zeros(
+                batch_size,
+                self.out_dim,
+                seq_len,
+                seq_len,
+                dtype=elements.dtype,
+                device=elements.device,
+            )
             y[:, :, i, j] = elements
             y[:, :, j, i] = elements
         else:
