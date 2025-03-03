abea87fa047cad1a7b8a81579a17b14919d6710c
kgreif <kevingreif24@gmail.com>
2025-03-03 05:45:26 -0800

    Train 3 epochs in restart test

--- tests/test_omnifold.py
+++ tests/test_omnifold.py
@@ -144,7 +144,7 @@ def test_restart(tmp_path):
     # it so we only run one iteration
     config.mod_config("checkpoint_dir", tmp_path)
     config.mod_config("num_iterations", 1)
-    config.mod_config("max_epochs", 1)
+    config.mod_config("max_epochs", 3)
 
     # Write the config to a file
     config.create_template(template_path=f"{tmp_path}/test_of.yml")
