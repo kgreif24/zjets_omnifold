10f3948a96b9ba26e0e57828af420afead6bc13f
kgreif <kevingreif24@gmail.com>
2025-04-25 13:19:28 -0700

    Filter more legacy kwargs in lightning module

--- lightning_eval.py
+++ lightning_eval.py
@@ -118,7 +118,6 @@ class OfEval:
         # Load model checkpoint
         self.model = LOfTransformer.load_from_checkpoint(
             check_path,
-            test_plots=self.test_dir if self.config.wandb else None,
             debug=self.config.debug,
             step=self.step,
         )
