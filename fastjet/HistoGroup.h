127e5a05440416eee929e4fe23c20005130cef4b
kgreif <kevingreif24@gmail.com>
2025-10-20 11:56:10 -0700

    Parallelize fastjet code, add data comparison to multifold script

--- fastjet/HistoGroup.h
+++ fastjet/HistoGroup.h
@@ -23,6 +23,7 @@ class HistoGroup {
         HistoGroup(string name = "default", int kinematic_region = 0);
         ~HistoGroup();
         void WriteHistos(TFile& foutput);
+        void MergeHistos(const HistoGroup& other);
 
         // Name for the histogram group, prepended to each histogram name
         string name;
@@ -43,15 +44,16 @@ class HistoGroup {
         // shared_ptr<TH1D> hLund_dR_R04;
         // shared_ptr<TH2D> hLund_plane_R04;
 
-        // // R=0.6 jets
-        // shared_ptr<TH1D> hpT_R06;
-        // shared_ptr<TH1D> hEEC_R06;
+        // R=0.6 jets
+        shared_ptr<TH1D> hm1_R06;
+        shared_ptr<TH1D> hpT_R06;
+        shared_ptr<TH1D> hEEC_R06;
         // shared_ptr<TH1D> hLund_z_R06;
         // shared_ptr<TH1D> hLund_dR_R06;
         // shared_ptr<TH2D> hLund_plane_R06;
 
         // R=1.0 jets
-        // shared_ptr<TH1D> hm1_R10;
+        shared_ptr<TH1D> hm1_R10;
         shared_ptr<TH1D> hpT_R10;
         shared_ptr<TH1D> hEEC_R10;
         // shared_ptr<TH1D> hLund_z_R10;
@@ -59,20 +61,21 @@ class HistoGroup {
         // shared_ptr<TH2D> hLund_plane_R10;
 
         // CA R=0.4 jets
+        shared_ptr<TH1D> hm1_CA04;
         shared_ptr<TH1D> hpT_CA04;
-        // shared_ptr<TH1D> hEEC_CA04;
+        shared_ptr<TH1D> hEEC_CA04;
         shared_ptr<TH1D> hmjj_CA04;
         shared_ptr<TH1D> hdRjj_CA04;
         shared_ptr<TH1D> hdyjj_CA04;
         shared_ptr<TH1D> hdphijj_CA04;
 
         // // CA R=0.6 jets
-        // shared_ptr<TH1D> hm1_CA06;
+        shared_ptr<TH1D> hm1_CA06;
         shared_ptr<TH1D> hpT_CA06;
 
-        // // KT R=0.4 jets
-        // shared_ptr<TH1D> hm1_KT04;
-        // shared_ptr<TH1D> hpT_KT04;
+        // KT R=0.4 jets
+        shared_ptr<TH1D> hm1_KT04;
+        shared_ptr<TH1D> hpT_KT04;
 
         // Event-level histograms
         shared_ptr<TH1D> hTEEC_collinear;
