Correct method bias calculation, tests passing

tests/test_plotter.py
@@ -34,14 +34,15 @@ def test_w1(tmp_path):
         tmp_path,
         labels=("Test1", "Test2"),
         verbosity=2,
+        normalize_targets=False,
     )
     start, end = plotter.wasserstein_distance(
         "weight",
         "./assets/wgts.npz",
         np.ones(100),
     )
-    assert np.isclose(start, 352.057, atol=0.1)
-    assert np.isclose(end, 364.483, atol=0.1)
+    assert np.isclose(start, 375.650, atol=0.1)
+    assert np.isclose(end, 402.671, atol=0.1)
 
 
 def test_apply_kinematic_cuts(tmp_path):

uncertainty_plotter.py
@@ -984,9 +984,11 @@ class UncertaintyPlotter(plotter.Plotter):
             rel_mbias = None
         else:
             # Find method bias
-            # Need to normalize target histogram to match source histogram
-            norm_factor = np.sum(source_hist) / np.sum(target_hist)
-            norm_target_hist = norm_factor * target_hist
+            if self.normalize_targets:
+                norm_factor = np.sum(source_hist) / np.sum(target_hist)
+                norm_target_hist = norm_factor * target_hist
+            else:
+                norm_target_hist = target_hist
             mbias = (source_hist - norm_target_hist) ** 2
             rel_mbias = np.sqrt(mbias) / norm_target_hist
 